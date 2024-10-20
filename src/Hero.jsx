97210404b39027ea96331fdc185a942b83232760
Mykhailo Shevchenko <shevchenko21021984@gmail.com>
2024-10-20 19:37:14 +0200

    fix content

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -6,16 +6,15 @@ const Hero = () => {
         <div className="hero-title">
           <h1>Contentful CMS</h1>
           <p>
-            This portfolio highlights a variety of projects, each representing a
-            different level of complexity. These projects demonstrate my growth
-            in web development, particularly in building responsive applications
-            that utilize modern technologies. From basic web apps like task
-            managers to more intricate platforms with dynamic content managed
-            via headless CMS systems, these projects reflect my ability to work
-            with both static and dynamic data, delivering high-quality,
-            functional applications. Explore the projects below for a deeper
-            look into the different features and approaches employed in each
-            one.
+            This page showcases a broad range of projects, illustrating various
+            levels of complexity and development challenges. Throughout these
+            projects, I have refined my ability to create responsive and
+            functional web applications using modern technologies. From simpler
+            task management tools to more advanced platforms featuring dynamic
+            content handled through headless CMS systems, these projects reflect
+            my versatility in working with both static and dynamic data. Take a
+            closer look at the projects below to explore the unique features and
+            techniques used in each one.
           </p>
         </div>
         <div className="img-container">
